61ec80788354f4b5c7800d3fd5a918ebe49213d0
Satharaka Nilmantha <satharakanilmantha1@gmail.com>
2025-03-08 08:52:38 +0530

    Create Server Side package

---
+++ src/main/java/com/example/SpiritX_Dev_Titans_01_Server_Side/SpiritXDevTitans01ServerSideApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpiritX_Dev_Titans_01_Server_Side;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiritXDevTitans01ServerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiritXDevTitans01ServerSideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpiritX_Dev_Titans_01_Server_Side/SpiritXDevTitans01ServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpiritX_Dev_Titans_01_Server_Side;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiritXDevTitans01ServerSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
