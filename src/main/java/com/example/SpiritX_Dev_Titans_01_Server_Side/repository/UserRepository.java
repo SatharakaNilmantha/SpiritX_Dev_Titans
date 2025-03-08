8617a2602f0c040afe90cb857c4992ed33fe4f74
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-03-08 09:48:18 +0530

    Implement UserRepository Interface

---
+++ src/main/java/com/example/SpiritX_Dev_Titans_01_Server_Side/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.SpiritX_Dev_Titans_01_Server_Side.repository;
+
+import com.example.SpiritX_Dev_Titans_01_Server_Side.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository  extends MongoRepository<User,String> {
+    Optional<User> findByUsername(String username);
+}
