ba94218f36323b6fb376776c76e7ccca033708f3
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-03-08 10:56:30 +0530

    Apllication.propertis file modifered

--- src/main/java/com/example/SpiritX_Dev_Titans_01_Server_Side/SpiritXDevTitans01ServerSideApplication.java
+++ src/main/java/com/example/SpiritX_Dev_Titans_01_Server_Side/SpiritXDevTitans01ServerSideApplication.java
@@ -2,8 +2,9 @@ package com.example.SpiritX_Dev_Titans_01_Server_Side;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpiritXDevTitans01ServerSideApplication {
 
 	public static void main(String[] args) {
